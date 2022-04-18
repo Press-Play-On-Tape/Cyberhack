ae213b882d0e45ac675dfe7751796291619044c6
Andrew Dent <dent.ace@gmail.com>
2022-04-18 23:47:12 +0100

    Optimize PPOT splash screen
    Tweaks save ~66 bytes.

--- Cyberhack/Cyberhack_SplashScreen.ino
+++ Cyberhack/Cyberhack_SplashScreen.ino
@@ -1,5 +1,8 @@
 #include "src/utils/Arduboy2Ext.h"
 
+static constexpr uint8_t ppotFrames = (1.6 *50); // 1.6s animation loop
+
+
 // ----------------------------------------------------------------------------
 //  Initialise state ..
 //
@@ -29,29 +32,49 @@ void splashScreen() {
     }
 
 
-    Sprites::drawOverwrite(32, 17, Images::PPOT, 0);
+    Sprites::drawOverwrite(32, 16, Images::PPOT, 0);
 
-    switch (arduboy.getFrameCount(48)) {
+    uint8_t p = 0x11; // Default pixel position 0 is hidden in the image
 
-        case 0 ... 11:
-            Sprites::drawOverwrite(91, 25, Images::PPOT_Arrow, 0);
+    switch (arduboy.getFrameCount(ppotFrames)) {
+
+        // 2nd quarter of frames
+        case (ppotFrames*1/4) ... (ppotFrames*2/4)-1:
+            p = 0x31; // Move pixel down to position 1
+            /*-fallthrough*/
+
+        // 1st quarter of frames
+        case 0 ... (ppotFrames*1/4)-1:
+            // Flash 'Play' arrow by clearing the image
+            // Overwrite directly to the screen buffer 
+            arduboy.sBuffer[91 +(3*WIDTH)] = 0x00;
+            arduboy.sBuffer[92 +(3*WIDTH)] = 0x00;
+            arduboy.sBuffer[93 +(3*WIDTH)] = 0x00;
+            arduboy.sBuffer[94 +(3*WIDTH)] = 0x00;
+            arduboy.sBuffer[95 +(3*WIDTH)] = 0x00;
             break;
 
-        case 12 ... 23:
-            Sprites::drawOverwrite(45, 28, Images::PPOT_Spindle, 0);
-            Sprites::drawOverwrite(91, 25, Images::PPOT_Arrow, 0);
-            arduboy.drawPixel(52, 30, WHITE);
+        // 3rd quarter of frames
+        case (ppotFrames*2/4) ... (ppotFrames*3/4)-1:
+            p = 0x51; // Move pixel down to position 2
             break;
 
-        case 24 ... 35:
-            arduboy.drawPixel(52, 31, WHITE);
+        // 4th quarter of frames
+        default:
+            p = 0x91; // Move pixel down to position 3
             break;
 
-        case 36 ... 47:
-            Sprites::drawOverwrite(45, 28, Images::PPOT_Spindle, 0);
-            arduboy.drawPixel(52, 32, WHITE);
-            break;
+    }
 
+    // Draw pixel to represent the tape spooling
+    // Render directly to the screen buffer 
+    arduboy.sBuffer[52 +(3*WIDTH)] = p; // Values 0x11, 0x31, 0x51, 0x91
+
+    // On even steps of pixel movement, update the spindle image
+    if ((p & 0xA0) == 0) {
+        // Render directly to the screen buffer 
+        arduboy.sBuffer[45 +(3*WIDTH)] = 0xA5;
+        arduboy.sBuffer[46 +(3*WIDTH)] = 0x95;
     }
 
 }
